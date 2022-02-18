a29a98091da92aaa6f4fe9ef19028c6e01738d78
Mező Dávid <mezo.david@autsoft.hu>
2022-02-18 11:41:19 +0100

    Angular update

--- src/app/flight/flight.module.ts
+++ src/app/flight/flight.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlightComponent } from './flight.component';
 import { FligthsListComponent } from './fligths-list/fligths-list.component';
-import {MatTableModule} from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [FlightComponent, FligthsListComponent],
